47a33abc013b776e31b863074786d2688e9968c8
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2022-01-09 15:50:28 +0800

    [frontend] add homepage

--- frontend/src/pages/switch.js
+++ frontend/src/pages/switch.js
@@ -1,9 +1,11 @@
-import TradePage from "./trade"
-import LoginPage from "./login"
-import RegisterPage from "./register"
-import { Routes, Route } from 'react-router-dom'
+import HomePage from "./home";
+import TradePage from "./trade";
+import LoginPage from "./login";
+import RegisterPage from "./register";
+import { Routes, Route } from 'react-router-dom';
 export default function SwitchPage() {
     return <Routes>
+        <Route path="/home" element={<HomePage/>}></Route>
         <Route path="/trade" element={<TradePage/>}></Route>
         <Route path="/register" element={<RegisterPage/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
