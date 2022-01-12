8abaea455c1a5389e2091ad21300eeaf27852388
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2022-01-13 05:39:34 +0800

    [frontend/home] add graphql interaction in record.js/strategy.js (not completely tested)

--- frontend/src/pages/home/strategy.js
+++ frontend/src/pages/home/strategy.js
@@ -12,7 +12,8 @@ import {
   STRATEGY_QUERY,
   RENAME_STRATEGY_MUTATION,
   DELETE_STRATEGY_MUTATION,
-  DELETE_RECORD_MUTATION
+  DELETE_RECORD_MUTATION,
+  DELETE_RECORD_BY_STRATEGY_ID_MUTATION
 } from "../../graphql";
 
 const Wrapper = styled.div`
@@ -36,35 +37,32 @@ const Title = styled.div`
 export default function Strategy({ username="" }) {
   const { loading, error, data } = useQuery(STRATEGY_QUERY, {variables: {id: ""}});
   const [allRecord, setAllRecord] = useState(true);
-  const [name, setName] = useState("");
+  const [strategyName, setStrategyName] = useState("");
   const [newStrategyName, setNewStrategyName] = useState("");
+  const [strategyID, setStrategyID] = useState("");
   const [showEditModal, setShowEditModal] = useState(false);
-  const [editedIndex, setEditedIndex] = useState(-1);
+  const [editedID, setEditedID] = useState("");
 
   console.log(loading);
   console.log(data);
   const renameStrategy = useMutation(RENAME_STRATEGY_MUTATION);
   const deleteStrategy = useMutation(DELETE_STRATEGY_MUTATION);
   const deleteRecord = useMutation(DELETE_RECORD_MUTATION);
-  // const [dataSource, setDataSource] = useState([{
-  //   key: 0,
-  //   name: `Strategy 0`,
-  //   roi: "30%",
-  //   action: 0,
-  // }]);
+  const deleteRecordByStrategyID = useMutation(DELETE_RECORD_BY_STRATEGY_ID_MUTATION);
+  const [dataSource, setDataSource] = useState([{
+    id: "8415d7ac-32ef-4ec8-805f-ee0491e73f0d",
+    name: `Strategy 0`
+  }]);
 
-  const handleDeleteStrategy = (idx) => { // TODO: should write back to database?
-    // console.log(`delete ${idx}`);
-    // console.log(dataSource);
-    // const newDataSource = dataSource.filter(item => item.key !== idx);
-    // console.log(newDataSource);
-    // setDataSource(newDataSource);
+  const handleDeleteStrategy = (id) => { // TODO: should write back to database?
+    console.log(`delete ${id}`);
+    deleteStrategy({variables: {id: id}});
+    deleteRecordByStrategyID({variables: {strategyID: id}});
   };
 
-  const handleRenameStrategy = (idx) => { // TODO: should write back to database?
-  };
-
-  const handleOk = () => {
+  const client = useApolloClient();
+  const handleRenameStrategy = async () => { // TODO: should write back to database?
+    // id => editedID
     if (newStrategyName === "") {
       displayStatus({
         type: "error",
@@ -72,23 +70,27 @@ export default function Strategy({ username="" }) {
       });
       return;
     }
+    // TODO: name can not be duplicated
+    const req = await client.query({
+      query: STRATEGY_QUERY,
+      variables: {id: ""}
+    });
+    // req contain all strategy name, we should check if there exists newStrategyName
 
-    // const {tmp_data} = useQuery(STRATEGY_QUERY, {variables: {id: newStrategyName}});
-    // if (tmp_data.GetStrategy.length !== 0)
+    console.log(newStrategyName);
+    renameStrategy({variables: {id: editedID, name: newStrategyName}});
 
-    // const objIndex = dataSource.findIndex(item => item.key === editedIndex);
-    // const newDataSource = dataSource;
-    // newDataSource[objIndex].name = newStrategyName;
-    // console.log(newStrategyName);
-    // setDataSource(newDataSource);
-    // setShowEditModal(false);
-    // setNewStrategyName(newStrategyName => "");
-    setEditedIndex(-1);
+    setShowEditModal(false);
+    setNewStrategyName("");
+    setEditedID("");
     displayStatus({
       type: "success",
       msg: "new strategy name is set!",
     });
-    // TODO: put edited strategy name back to database
+  };
+
+  const handleOk = () => {
+    handleRenameStrategy();
   };
   const handleCancel = () => {
     setShowEditModal(false);
@@ -99,16 +101,16 @@ export default function Strategy({ username="" }) {
       dataIndex: "name",
       width: 300,
       render: (name) => (
-        <Button type="link" onClick={() => {setAllRecord(false); setName(name);}}>{name}</Button>
+        <Button type="link" onClick={() => {setAllRecord(false); setStrategyName(name);}}>{name}</Button>
       ),
     },
     {
       title: "",
-      dataIndex: "action",
-      render: (action) => (
+      dataIndex: "id",
+      render: (id) => (
         <>
-          <EditOutlined onClick={() => {setShowEditModal(true); setEditedIndex(action);}} />
-          <DeleteOutlined onClick={() => {handleDeleteStrategy(action);}} />
+          <EditOutlined onClick={() => {setShowEditModal(true); setEditedID(id);}} />
+          <DeleteOutlined onClick={() => {handleDeleteStrategy(id);}} />
         </>
       ),
     }
@@ -131,7 +133,7 @@ export default function Strategy({ username="" }) {
             <Title>
               <h1>{username}'s Strategies</h1>
             </Title>
-            <Table columns={columns} dataSource={data.GetStrategy} onRow={record => ({
+            <Table columns={columns} dataSource={dataSource} onRow={record => ({
               // onClick: () => {setAllRecord(false); setIndex(record.key);},
               onClick: () => {},
             })}/>
@@ -150,7 +152,7 @@ export default function Strategy({ username="" }) {
           </>
         ) : (
           <>
-            <Record strategy={name} setName={setName} allRecord={allRecord} setAllRecord={setAllRecord} />
+            <Record strategyName={strategyName} setStrategyName={setStrategyName} strategyID={strategyID} setStrategyID={setStrategyID} allRecord={allRecord} setAllRecord={setAllRecord} />
           </>
         )
       }
