284b3d487485a28221ef82ada5e08e6904cfe93b
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2022-01-13 17:45:30 +0800

    [frontend] fix strategy.js, add No.

--- frontend/src/pages/home/strategy.js
+++ frontend/src/pages/home/strategy.js
@@ -45,10 +45,10 @@ export default function Strategy({ username="" }) {
 
   console.log(loading);
   console.log(data);
-  const renameStrategy = useMutation(RENAME_STRATEGY_MUTATION);
-  const deleteStrategy = useMutation(DELETE_STRATEGY_MUTATION);
-  const deleteRecord = useMutation(DELETE_RECORD_MUTATION);
-  const deleteRecordByStrategyID = useMutation(DELETE_RECORD_BY_STRATEGY_ID_MUTATION);
+  const [renameStrategy] = useMutation(RENAME_STRATEGY_MUTATION);
+  const [deleteStrategy] = useMutation(DELETE_STRATEGY_MUTATION);
+  const [deleteRecord] = useMutation(DELETE_RECORD_MUTATION);
+  const [deleteRecordByStrategyID] = useMutation(DELETE_RECORD_BY_STRATEGY_ID_MUTATION);
   const [dataSource, setDataSource] = useState([{
     id: "8415d7ac-32ef-4ec8-805f-ee0491e73f0d",
     name: `Strategy 0`
@@ -62,7 +62,6 @@ export default function Strategy({ username="" }) {
 
   const client = useApolloClient();
   const handleRenameStrategy = async () => { // TODO: should write back to database?
-    // id => editedID
     if (newStrategyName === "") {
       displayStatus({
         type: "error",
@@ -71,13 +70,12 @@ export default function Strategy({ username="" }) {
       return;
     }
     // TODO: name can not be duplicated
-    const req = await client.query({
-      query: STRATEGY_QUERY,
-      variables: {id: ""}
-    });
+    // const req = await client.query({
+    //   query: STRATEGY_QUERY,
+    //   variables: {id: ""}
+    // });
     // req contain all strategy name, we should check if there exists newStrategyName
 
-    console.log(newStrategyName);
     renameStrategy({variables: {id: editedID, name: newStrategyName}});
 
     setShowEditModal(false);
@@ -97,9 +95,15 @@ export default function Strategy({ username="" }) {
   };
   const columns = [
     {
+      title: "No.",
+      dataIndex: "num",
+      align: "center"
+    },
+    {
       title: "Strategy Name",
       dataIndex: "name",
       width: 300,
+      align: "center",
       render: (name) => (
         <Button type="link" onClick={() => {setAllRecord(false); setStrategyName(name);}}>{name}</Button>
       ),
@@ -133,10 +137,13 @@ export default function Strategy({ username="" }) {
             <Title>
               <h1>{username}'s Strategies</h1>
             </Title>
-            <Table columns={columns} dataSource={dataSource} onRow={record => ({
-              // onClick: () => {setAllRecord(false); setIndex(record.key);},
-              onClick: () => {},
-            })}/>
+            {
+              loading ? <p>Loading...</p> : 
+              <Table columns={columns} dataSource={data.GetStrategy.map((item, index) => {return {...item, num: index + 1};})} onRow={record => ({
+                // onClick: () => {setAllRecord(false); setIndex(record.key);},
+                onClick: () => {},
+              })}/>
+            }
             <Modal title="Edit strategy name here" visible={showEditModal} onOk={handleOk} onCancel={handleCancel} >
               <Input 
                 placeholder="new strategy name"
