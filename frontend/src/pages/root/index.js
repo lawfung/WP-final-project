fe0d5dcae46c92c5d515d97f66c3534c45e0b935
boook <boook64@gmail.com>
2022-01-15 18:22:12 +0800

    Add react

--- frontend/src/pages/root/index.js
+++ frontend/src/pages/root/index.js
@@ -1,4 +1,6 @@
+import React from "react";
 import rootImage from './root_page.jpeg';
+
 export default function RootPage(){
     return <div style={{overflow: "hidden", display: "flex", flexDirection: "column"}}>
         <img src={rootImage} alt="abc"/>
