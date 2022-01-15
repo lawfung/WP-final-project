e98af044502a849456a5639aafad23d896aa23ff
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2022-01-15 11:57:51 +0800

    [frontend/graphql] fix payload bug

--- frontend/src/graphql/mutations.js
+++ frontend/src/graphql/mutations.js
@@ -13,8 +13,8 @@ export const RENAME_STRATEGY_MUTATION = gql`
 `;
 
 export const CREATE_RECORD_MUTATION = gql`
-  mutation ($strategyName: String!, $startTime: Int!, $endTime: Int!, $start: Float!, $end: Float!, $high: Float!, $low: Float!, $cookie: String!, $cookie: String!) {
-    CreateRecord(strategyName: $strategyName, startTime: $startTime, endTime: $endTime, start: $start, end: $end, high: $high, low: $low, cookie: $cookie, cookie: $cookie)
+  mutation ($strategyName: String!, $startTime: Int!, $endTime: Int!, $start: Float!, $end: Float!, $high: Float!, $low: Float!, $cookie: String!) {
+    CreateRecord(strategyName: $strategyName, startTime: $startTime, endTime: $endTime, start: $start, end: $end, high: $high, low: $low, cookie: $cookie)
   }
 `;
 
